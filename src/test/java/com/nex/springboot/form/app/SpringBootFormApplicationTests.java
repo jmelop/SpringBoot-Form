716960fe1f1fc46360055b3030f56c323ba2b22c
Juan Melo Pérez <juanmlstm@gmail.com>
2022-02-09 20:48:49 +0000

    First commit

---
+++ src/test/java/com/nex/springboot/form/app/SpringBootFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nex.springboot.form.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
